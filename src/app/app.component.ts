797dbd3c7d4f8560a32ca63faa2df8a1bc1336b7
Hugo Peres <hugo-pdf@hotmail.com>
2020-03-20 15:39:44 -0300

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { BuiltinType } from '@angular/compiler';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,28 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+
   title = 'vuzix';
+
+  ngOnInit(): void {
+    //Called after the constructor, initializing input properties, and the first call to ngOnChanges.
+    //Add 'implements OnInit' to the class.
+
+    // bs = 8; enter = 13; delete = 46;  aright = 39;  aleft = 37;
+    
+    window.addEventListener('keyup', ($key) => {
+      if ($key.keyCode == 39) {
+        window.document.getElementById('confirm').click();        
+        console.log(12)
+      }
+      console.log($key)
+    });
+    
+    // window.document.addEventListener('keydown') = function($key) {
+    //   console.log($key);
+    // }
+
+  }
+
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,25 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { ServiceWorkerModule } from '@angular/service-worker';
+import { environment } from '../environments/environment';
+import { CameraComponent } from './components/camera/camera.component';
+import { HeaderComponent } from './components/header/header.component';
+import { ButtonNavComponent } from './components/button-nav/button-nav.component';
+import { PictureComponent } from './components/picture/picture.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CameraComponent,
+    HeaderComponent,
+    ButtonNavComponent,
+    PictureComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    // ServiceWorkerModule.register('ngsw-worker.js', { enabled: environment.production })
+    ServiceWorkerModule.register('ngsw-worker.js', { enabled: true})
   ],
   providers: [],
   bootstrap: [AppComponent]
